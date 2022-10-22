350616a81967d44ab3926f11714c6dd41788e84b
Carlos E. Valenzuela C <89716956+CarlosVlaCor@users.noreply.github.com>
2022-10-22 15:03:33 -0600

    Inicio del proyecto del Sistema de Control de Egresados

---
+++ src/main/java/com/triplev/controlegresados/ControlEgresadosApplication.java
@@ -0,0 +1,13 @@
+package com.triplev.controlegresados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlEgresadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlEgresadosApplication.class, args);
+    }
+
+}
